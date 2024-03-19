caf652d14f22b4e5d18bb3c3519d56aef3d8a461
You-are-empty <56483245+You-are-empty@users.noreply.github.com>
2024-03-19 09:26:19 +0100

    method findMax added

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -7,5 +7,20 @@
     }
     return res/input.Length;
 }
-int[] arr = new []{1, 12, 0, 3, 4, 1};
-Console.WriteLine(average(arr));
+
+static int findMax(int[] input)
+{
+    int max = input[0];
+    foreach (var v in input)
+    {
+        if (v>max)
+        {
+            max = v;
+        }
+    }
+
+    return max;
+}
+
+int[] arr = new[] { 12, 0, 1, 4, 34, -1, 5 };
+Console.WriteLine(findMax(arr));
